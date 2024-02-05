40613e730db7447d66bec243a6dd3239df5c855c
Ciwy <c.walter@afk-dev.de>
2024-02-05 19:09:29 +0100

    maintenance: use constants instead of magic numbers

--- src/apps/users/tests/test_api.py
+++ src/apps/users/tests/test_api.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.test import TestCase
 from django.urls import reverse
+from rest_framework import status
 
 from apps.users.models import CustomUser
 
@@ -19,26 +20,26 @@ class AuthenticationTest(TestCase):
     def test_get_token_without_data_returns_status_400(self):
         response = self.client.post(self.token_url, {})
 
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_get_token_with_invalid_data_returns_status_400(self):
+    def test_get_token_with_invalid_data_returns_status_401(self):
         response = self.client.post(
             self.token_url, {"email": "", "password": ""}, format="json"
         )
 
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_get_token_with_valid_data_returns_status_200(self):
         response = self.client.post(
             self.token_url, {"email": SUPERUSER_EMAIL, "password": SUPERUSER_PASSWORD}
         )
 
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_protected_endpoint_without_token_returns_status_401(self):
         url = reverse("users:protected")
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_protected_endpoint_with_valid_token_returns_status_200(self):
         response = self.client.post(
@@ -48,4 +49,4 @@ class AuthenticationTest(TestCase):
         token = response.data[settings.API_TOKEN_NAME]
         url = reverse("users:protected")
         response = self.client.get(url, HTTP_AUTHORIZATION=f"Bearer {token}")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
