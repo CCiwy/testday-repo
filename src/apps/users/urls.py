457c873cb10e973b06e5422258c876961dbf70f4
Ciwy <c.walter@afk-dev.de>
2024-02-05 19:21:19 +0100

    add: 'api' prefix to api urls

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', views.login_view, name='login'),
     path('restricted-content/', views.restricted_content, name='restricted_content'),
     path('logout/', views.logout_view, name='logout'),
-    path('token/', views.get_token, name='token'),
-    path('protected/', views.protected_endpoint, name='protected')
+    path('api/token/', views.get_token, name='token'),
+    path('api/protected/', views.protected_endpoint, name='protected')
 
 ]
