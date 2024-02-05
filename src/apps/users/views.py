0e1f478dd4f4c65e67bd73b205de0b95cd06ce7f
Ciwy <c.walter@afk-dev.de>
2024-02-05 15:29:04 +0100

    add: user login view

--- src/apps/users/views.py
+++ src/apps/users/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('restricted_content')
+
+        return render(request, 'users/login.html')
