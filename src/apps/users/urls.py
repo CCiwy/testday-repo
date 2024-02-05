d218856dfd2af20770100829c93e5b8cf7484463
Ciwy <c.walter@afk-dev.de>
2024-02-05 16:01:44 +0100

    add: users urls

---
+++ src/apps/users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('restricted-content/', views.restricted_content, name='restricted_content'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    ]
