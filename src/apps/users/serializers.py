fddf7ec98b079cae3316d98f97c0787504617bac
Ciwy <c.walter@afk-dev.de>
2024-02-05 20:38:45 +0100

    add: get token serializer for user data

--- src/apps/users/serializers.py
+++ src/apps/users/serializers.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
 
 
+class GetTokenSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=255)
+
 class TokenSerializer(serializers.Serializer):
     access = serializers.CharField(max_length=255)
