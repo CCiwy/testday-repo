0568c64d8747733f76d49c6d223271b6fb0f55c2
Ciwy <c.walter@afk-dev.de>
2024-02-05 18:25:22 +0100

    add: api urls

--- src/apps/users/urls.py
+++ src/apps/users/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 app_name = 'users'
 
+
 urlpatterns = [
     path('', views.login_view, name='login'),
     path('restricted-content/', views.restricted_content, name='restricted_content'),
     path('logout/', views.logout_view, name='logout'),
-    ]
+    path('token/', views.get_token, name='token'),
+    path('protected/', views.protected_endpoint, name='protected')
+
+]
