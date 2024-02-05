15e6949a4308ed551009a687bb023793f8a3285d
Ciwy <c.walter@afk-dev.de>
2024-02-05 21:40:44 +0100

    fix: cannot unpack tuple 'token' if not exists'

--- src/apps/users/authentication.py
+++ src/apps/users/authentication.py
@@ -5,10 +5,10 @@ from django.contrib.auth import get_user_model
 
 class EmailTokenAuthentication(TokenAuthentication):
     def authenticate_credentials(self, key):
-        UserModel = get_user_model()
         try:
-            user, _ = Token.objects.get(key=key)
-        except UserModel.DoesNotExist:
+            token = Token.objects.get(key=key)
+            user = token.user
+        except Token.DoesNotExist:
             return None
 
         if not user.is_active:
